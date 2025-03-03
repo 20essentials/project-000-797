d8a48d9a9f1504610edbb5189ae41d277dc0e3b7
20essentials <s0l0estudi4r@gmail.com>
2025-03-03 19:03:16 +0100

    v0.0.1

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Foto Preview](preview/n797.avif)](https://20essentials.github.io/project-000-797)
+
+<div align="center" style="display: flex; justify-content: center;">
+  <a  href="https://github.com/20essentials/project-000-796" target="_blank">&#8592;</a>
+  &nbsp;&nbsp;
+  <a  href="https://github.com/20essentials/project-000-798" target="_blank">&#8594;</a>
+</div>
---
+++ script.js
@@ -0,0 +1,64 @@
+const Galaxy = class {
+  constructor() {
+    this.MAX_STARS = 750;
+    this.MAX_SPEED = 6;
+    this.COLORS = ['#444', '#888', '#FFF'];
+
+    this.canvas = document.querySelector('#galaxy');
+    this.ctx = this.canvas.getContext('2d', { alpha: false });
+
+    window.onresize = this.expandCanvas;
+    this.expandCanvas();
+    this.createStars();
+  }
+
+  expandCanvas() {
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+  }
+
+  clearScreen() {
+    this.ctx.fillStyle = '#000';
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  createStars() {
+    this.stars = [];
+    for (let i = 0; i < this.MAX_STARS; i++) {
+      this.stars.push({
+        x: ~~(Math.random() * this.canvas.width),
+        y: ~~(Math.random() * this.canvas.height),
+        speed: ~~(1 + Math.random() * this.MAX_SPEED),
+        color: ~~(Math.random() * this.COLORS.length)
+      });
+    }
+  }
+
+  loop() {
+    requestAnimationFrame(() => {
+      this.loop();
+    }, this.canvas);
+    this.update();
+    this.render();
+  }
+
+  update() {
+    for (let i = 0; i < this.MAX_STARS; i++) {
+      this.stars[i].x -= this.stars[i].speed;
+      this.stars[i].x = this.stars[i].x < 0 ? this.canvas.width : this.stars[i].x;
+    }
+  }
+
+  render() {
+    this.clearScreen();
+    for (let i = 0; i < this.MAX_STARS; i++) {
+      var s = this.stars[i];
+      this.ctx.lineWidth = 1;
+      this.ctx.strokeStyle = this.COLORS[this.stars[i].color];
+      this.ctx.strokeRect(s.x, s.y, 1, 1);
+    }
+  }
+};
+
+const galaxy = new Galaxy();
+galaxy.loop();
